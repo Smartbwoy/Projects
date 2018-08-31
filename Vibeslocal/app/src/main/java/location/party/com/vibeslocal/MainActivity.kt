1d8da650e0dd0bcf5cf5b1dadf616c1d1a55121e
Miguel Jackson <rrennie66@gmail.com>
2018-08-30 22:00:12 -0500

    new mock ups

---
+++ Vibeslocal/app/src/main/java/location/party/com/vibeslocal/MainActivity.kt
@@ -0,0 +1,35 @@
+package location.party.com.vibeslocal
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        // Example of a call to a native method
+        sample_text.text = stringFromJNI()
+        val intent = Intent(this, LoginActivity::class.java)
+        // start your next activity
+        startActivity(intent)
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
